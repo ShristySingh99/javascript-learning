28fed0a1c5adc27a4bfb8f4be4a689de93efcea8
ShristySingh99 <152967083+ShristySingh99@users.noreply.github.com>
2025-09-03 17:44:02 +0000

    primitivedatatype lesson

---
+++ 01_basics/datatype.js
@@ -0,0 +1,58 @@
+
+//data types
+console.log("hey siri");
+const accoundId = 1232;
+let accountEmail = "sirir@fffg";
+var accPass = "2345";
+accCity = "dehri";
+//accoundId= 3455;
+
+
+//console.table([accoundId, accountEmail, accPass, accCity]);
+
+let counter = 100;
+//console.log(counter++)
+
+let logg = true;
+//console.log(+logg)
+
+// console.log(222>" 32")
+// console.log(null == 0)
+// console.log(null <0)
+// console.log(null >0)
+// console.log(null === undefined)
+// console.log("2"==2);
+// console.log("2"=== 2)
+
+
+let bigInt = 12324253222234566666665;
+//console.log(typeof bigInt);
+
+let sym = Symbol('123');
+//console.log(typeof sym)
+
+// let arr = [1,34,42.2,3]
+// console.log(typeof arr);
+
+// let myobj = {
+//    name: " siri",
+//    age : 19
+// }
+// console.log(myobj);
+// console.log(typeof myobj);
+
+// let fun = function(){
+//    console.log("function called")
+// }
+// console.log(fun);
+// console.log(typeof fun);
+
+
+//date and time
+
+let newDate = new Date();
+//console.log(newDate.toISOString());
+
+
+let myDate = new Date("2025-01-2");
+console.log(myDate.toDateString());
